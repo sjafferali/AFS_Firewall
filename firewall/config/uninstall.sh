1863c8782b7343f4f03d4c58aef771b9c5866278
Samir Jafferali <samir.jafferali@gmail.com>
2015-10-21 06:15:09 -0500

    Updated uninstall script for RHEL 7

--- firewall/config/uninstall.sh
+++ firewall/config/uninstall.sh
@@ -13,9 +13,12 @@ fi
 
 bak_dir="/etc/firewall_"`date +"%Y-%m-%d-%T"`
 /etc/firewall/firewall flush
+/etc/firewall/firewall stop
 echo -ne "[*] Removing firewall... "
 mv /etc/firewall $bak_dir
-mv /etc/init.d/firewall $bak_dir/firewall_init
+rm -f /etc/init.d/firewall
+mv /usr/lib/systemd/system/firewall.service $bak_dir
+systemctl daemon-reload
 mv /etc/.firewall $bak_dir
 rm -f /usr/sbin/firewall
 echo Done
